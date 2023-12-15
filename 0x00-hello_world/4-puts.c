26593a50884f58204468b3da8cdb2d0e5f0530bf
winifredogbeiwi <imadeogbeiwi@gmail.com>
2023-12-14 19:54:57 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-* main - A program that prints a line with puts function
-* Return: 0 (Success) 
-*/
+ * main -  C program that print line using put function
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+printf("\"Programming is like building a multilingual puzzle");
 return (0);
 }
